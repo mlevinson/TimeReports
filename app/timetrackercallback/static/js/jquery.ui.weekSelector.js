c7881b785ef0d12d467a162972aaa50e80d5ad6c
Lakshmi Vyas <lakshmi.vyas@gmail.com>
2010-02-24 17:42:02 +0530

    Fixed left / right arrows in week selector

--- app/timetrackercallback/static/js/jquery.ui.weekSelector.js
+++ app/timetrackercallback/static/js/jquery.ui.weekSelector.js
@@ -1,7 +1,7 @@
-(function($) { 
+(function($) {
 $.widget("ui.weekSelector",{
 
-    _init: function(){     
+    _init: function(){
             var widget = this;
             var displayHtml = "<a href=\"#\" id=\"previous_week\" class=\"icon previous\"></a>\
                                     <a id=\"week_selector\" class=\"dispatchable\">\
@@ -9,40 +9,45 @@ $.widget("ui.weekSelector",{
                                         <span class=\"icon arrow_down\"></span>\
                                     </a>\
                                <a href=\"#\" id=\"next_week\" class=\"icon next\"></a>";
-            
-            $(widget.element[0]).append(displayHtml);                              
+
+            $(widget.element[0]).append(displayHtml);
             $("#previous_week").unbind("click").bind("click", function(){
-                var newStartDate = $("#week_selector").dpGetSelected().clone();
-                newStartDate.moveToDayOfWeek(1, -1); 
+                var newStartDate = $("#week_selector").dpGetSelected()[0].clone();
+                newStartDate.moveToDayOfWeek(1, -1);
                 $("#week_selector").dpSetSelected(newStartDate.toString("dd/MM/yyyy"));
-            });           
+                return false;
+            });
 
             $("#next_week").unbind("click").bind("click", function(){
-                var newStartDate = idget.options.weekStartDate.clone();
-                newStartDate.moveToDayOfWeek(1, 1); 
+                var newStartDate = widget.options.weekStartDate.clone();
+                newStartDate.moveToDayOfWeek(1, 1);
                 $("#week_selector").dpSetSelected(newStartDate.toString("dd/MM/yyyy"));
+                return false;
             });
 
             $("#week_selector").datePicker({
-                startDate:"01/01/1996", 
-                selectWeek:true, 
+                startDate:"01/01/1996",
+                selectWeek:true,
                 createButton:false
-            })            
-            .unbind("click").bind("click", function(){$("#week_selector").dpDisplay();})
+            })
+            .unbind("click").bind("click", function(){
+                $("#week_selector").dpDisplay();
+                return false;
+             })
             .unbind("dateSelected").bind("dateSelected",function(e, selectedDate, $td){
-                var displayText = selectedDate.toString("dd MMM yyyy"); 
+                var displayText = selectedDate.toString("dd MMM yyyy");
                 displayText += " - ";
                 displayText += selectedDate.clone().addDays(6).toString("dd MMM yyyy");
                 $("#week_selector_name").text(displayText);
                 $(widget.element[0]).trigger("dateSelected", selectedDate);
              })
-             .dpSetSelected(widget.options.weekStartDate.toString("dd/MM/yyyy"));        
-    }                                                           
-});      
-    
+             .dpSetSelected(widget.options.weekStartDate.toString("dd/MM/yyyy"));
+    }
+});
+
 $.extend($.ui.ringceMonthSelector, {
    defaults: {
        weekStartDate: Date.today()
    }
- });          
+ });
 })(jQuery);
