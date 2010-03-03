e12eddc9fdbb8901ff1ab27a6eb9074298e5a92e
Lakshmi Vyas <lakshmi.vyas@gmail.com>
2010-03-03 23:23:46 +0530

    Compelted Team Selector shows labels if zero or one item is selected

--- app/timetrackercallback/static/js/jquery.ui.oDeskSelectWidget.js
+++ app/timetrackercallback/static/js/jquery.ui.oDeskSelectWidget.js
@@ -3,6 +3,8 @@ $.widget("ui.oDeskSelectWidget",{
     _init: function(){
         var widget = this;
         var select = this.element[0];
+        $(select).after('<span style="display:none" class="' + widget.options.noItemStyle + '"></span>');
+        $(select).after('<span style="display:none" class="' + widget.options.singleItemStyle + '"></span>');
         $(select).unbind("change").bind("change", function(){
             $(this).children("option:selected").each(function(){
                 var selectedReference =  $(this).val();
@@ -16,7 +18,10 @@ $.widget("ui.oDeskSelectWidget",{
     },
     _selectionChanged: function(element){
         var widget = this;
-        if(!element || !$(element).length) return;
+        if(!element || !$(element).length) {
+            $(widget.element[0]).trigger("selectionChanged", null);
+            return;
+        };
          $(element).attr("selected", true);
          var obj = widget.options.stateVariable;
          obj.reference = $(element).val();
@@ -42,8 +47,36 @@ $.widget("ui.oDeskSelectWidget",{
     setDefaults: function(select){
         this._selectionChanged($(this.element[0]).children("option:first-child"));
     },
+    _createNoItemLabel: function(){
+        var widget = this;
+        var select = this.element[0];
+        $(select).hide();
+        $(select).nextAll("." + widget.options.singleItemStyle).hide();
+        $(select).nextAll("." + widget.options.noItemStyle)
+            .show()
+            .text(widget.options.noItemsText);
+        var obj = widget.options.stateVariable;
+        obj.reference = null;
+        obj.id = 0;
+        obj.name = "";
+        $(select).trigger("selectionChanged", null);
+    },
+    _createSingleLabel: function(obj){
+        var widget = this;
+        var select = this.element[0];
+        $(select).hide();
+        $(select).nextAll("." + widget.options.noItemStyle).hide();
+        $(select).nextAll("." + widget.options.singleItemStyle)
+            .text(widget.options.useDisplayName ? obj.getDisplayName() : obj.name)
+            .show();
+        widget.options.stateVariable.reference = obj.reference;
+        widget.options.stateVariable.id = obj.id;
+        widget.options.stateVariable.name = obj.name;
+        $(select).trigger("selectionChanged", obj);
+    },
     populate: function(params){
        if(!this.options.stateVariable || !this.options.report || !this.options.service)  return;
+       var select = this.element[0];
        var options = "";
        if (this.options.includeAllOption){
            options += "<option id='" +
@@ -53,6 +86,17 @@ $.widget("ui.oDeskSelectWidget",{
 
        var widget = this;
        this.options.service(this.options.report, function(objs){
+
+           if(objs.length == 1){
+               widget._createSingleLabel(objs[0]);
+               return;
+           } else if (objs.length == 0){
+               widget._createNoItemLabel();
+               return;
+           }
+           $(select).nextAll(widget.options.noItemStyle).hide();
+           $(select).nextAll(widget.options.singleItemStyle).hide();
+           $(select).show();
            var selectedObj = null;
            $.each(objs, function(i, obj){
                var selected = false;
@@ -89,7 +133,10 @@ $.extend($.ui.oDeskSelectWidget, {
        useDisplayName:  false,
        all_option_text: "All",
        all_option_id:   "All",
-       includeAllOption: true
+       includeAllOption: true,
+       noItemsText: "Nothing to select",
+       singleItemStyle: "single_value",
+       noItemStyle: "no_value"
    }
  });
 })(jQuery);
